5f8eeab16319df84d2296c5d531cabc307fb19c1
atulkumar.m@livspace.com <atulkumar.m@livspace.com>
2024-04-05 00:09:34 +0530

    updated base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import { splitVendorChunkPlugin } from 'vite';
 import { defineConfig } from 'vite';
 export default defineConfig( {
-	base: '/',
+	base: '/TestDriveDemo/',
 	build: {
 		rollupOptions: {
 			output: {
